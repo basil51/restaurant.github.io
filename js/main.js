87cd6ea0cb1dfa879deec3b315e5c67985329023
Basel Emran Jerjawi <basel51@gmail.com>
2019-03-10 22:58:03 +0200

    finsh main page css starting restaurant

--- js/main.js
+++ js/main.js
@@ -158,27 +158,31 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
+  const link = document.createElement('a');
+  link.href = DBHelper.urlForRestaurant(restaurant);
+  li.append(link)
+
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  link.append(image);
+
+  const info = document.createElement('div');
+  info.className = "info";
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  info.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  info.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  info.append(address);
 
-  const more = document.createElement('a');
-  more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  link.append(info);
 
   return li
 }
